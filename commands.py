bc4182535db564c74fce314dcf9510b831cfd064
Lev <cerezovlev@gmail.com>
2024-03-06 19:07:00 +0700

    a

--- commands.py
+++ commands.py
@@ -8,13 +8,13 @@ def read_write(m_text, orders_df):
     row = m_text[1]
 
     if len(m_text) == 2:
-        status = orders_df.loc[orders_df['room'] == row, column].values
+        status = orders_df.loc[orders_df[orders_df.columns[0]] == row, column].values
     else:
         input_info = m_text[2]
         if(not input_info.isdigit()):
             raise Exception("can be only number")
 
-        status = orders_df.loc[orders_df['room'] == row, column] = input_info
+        status = orders_df.loc[orders_df[orders_df.columns[0]] == row, column] = input_info
         orders_df.to_excel(file_path, sheet_name=sheet_name, index=False, engine="openpyxl")
             
     if len(status) > 0:
@@ -24,28 +24,41 @@ def read_write(m_text, orders_df):
 
 def moved(m_text, orders_df):
     from globals import message_g
+    
+
     column = m_text[1]
     row = m_text[2]
     row2 = m_text[3]
-
+    a = orders_df.loc[orders_df[orders_df.columns[0]] == row, column]
+    b = orders_df.loc[orders_df[orders_df.columns[0]] == row2, column]
+    
     if (not row in getattr(orders_df, orders_df.columns[0]).values or not row2 in getattr(orders_df, orders_df.columns[0]).values ):
         raise Exception(f"room not exist")
-    if(orders_df.loc[orders_df['room'] == row, column].values == 0):
+    if(a.values[0] == '0'):
        raise Exception(f"haven't {column} in the room")
     
-    orders_df.loc[orders_df['room'] == row, column] -=1
-    orders_df.loc[orders_df['room'] == row2, column] += 1
-    orders_df.to_excel(file_path, sheet_name=sheet_name, index=False)    
+   
+    
+    if(b.values[0] == "0"):
+        orders_df.loc[orders_df[orders_df.columns[0]] == row2, column] = a.values[0]
+    else:
+        c = f", {a.values[0]}"
+        orders_df.loc[orders_df[orders_df.columns[0]] == row2, column] = str(b.values[0])
+        orders_df.loc[orders_df[orders_df.columns[0]] == row2, column] += c
+    orders_df.loc[orders_df[orders_df.columns[0]] == row, column] = "0"
 
-    bot.send_message(message_g.chat.id, f"fine")
+    orders_df.to_excel(file_path, sheet_name=sheet_name, index=False, engine="openpyxl")    
+
+    see_full_table(orders_df)
+    #bot.send_message(message_g.chat.id, f"fine")
 
 def see_column(text, orders_df, min_number = 0):
     from globals import message_g
     column = text[1]
     output = ''
 
-    if(len(text) == 3): min_number = int(text[2])
-    a = orders_df.loc[orders_df[column] > min_number, [column, 'room']]
+    #if(len(text) == 3): min_number = int(text[2])
+    a = orders_df.loc[orders_df[column] != "0", [column, orders_df.columns[0]]]
     
     for i in range(0, a.shape[0]):
         output += str(a.room.values[i])
